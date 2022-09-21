1b93da81482d8952575781f08b23d7d25b1f5a1d
DengCong <dengc99@163.com>
2022-09-21 20:58:26 +0800

    correct file upload and change file area display

--- src/components/ContentArea.vue
+++ src/components/ContentArea.vue
@@ -2,9 +2,6 @@
   <div class="card-header">
     <div class="btn-group" role="group" aria-label="Basic outlined example">
       <div class="upload-btn">
-        <!-- <button type="button" class="btn btn-outline-primary">上传</button> -->
-        <!-- <label for="upload" class="btn btn-outline-primary">上传</label> -->
-        <!-- <input type="file" class="upload-file" id="upload" @change="upload"> -->
       </div>
       <div class="new-dir-btn">
         <button type="button" class="btn btn-outline-primary">新建文件夹</button>
@@ -14,7 +11,7 @@
       </div>
     </div>
   </div>
-  <div class="card-body path-navigate">
+  <!-- <div class="card-body path-navigate">
     <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
       <ol class="breadcrumb">
         <li class="breadcrumb-item" style="cursor: pointer;" @click="getHomeDirFiles">
@@ -22,22 +19,23 @@
         </li>
       </ol>
     </nav>
-  </div>
+  </div> -->
   <div class="card-body content-area">
-    <ul v-if="homeDirList.length" class="list-group">
-      <li v-for="item of homeDirList" :key="item.id">
-        {{item}}
-      </li>
-    </ul>
-    <ul v-if="homeFileList.length" class="list-group">
-      <li v-for="item of homeFileList" :key="item.id">
-        {{item}}
-      </li>
-    </ul>
+    <el-table ref="multipleTableRef" :data="tableData" height="250" style="width: 100%"
+      @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55" />
+      <el-table-column property="name" label="文件名" width="400" />
+      <el-table-column label="修改日期" width="200">
+        <template #default="scope">{{ scope.row.date }}</template>
+      </el-table-column>
+      <el-table-column property="address" label="大小" width="120" />
+    </el-table>
   </div>
-  <div class="card-body">
-    <el-upload name="file" class="upload-demo" drag action="none" :auto-upload="false" :on-change="uploadFile">
-      <i class="el-icon-upload"></i>
+  <div class="card-body upload-area">
+    <el-upload class="upload-demo" drag :action="uploadUrl">
+      <el-icon class="el-icon--upload">
+        <upload-filled />
+      </el-icon>
       <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
     </el-upload>
   </div>
@@ -47,13 +45,49 @@
 import $ from 'jquery';
 import { useStore } from 'vuex';
 import { reactive } from 'vue';
+import { UploadFilled } from '@element-plus/icons-vue'
 
 export default {
   name: "ContentArea",
+  components: {
+    UploadFilled,
+  },
   setup() {
     const store = useStore();
     let homeFileList = reactive([]);
     let homeDirList = reactive([]);
+    const tableData = [
+      {
+        date: '2016-05-07',
+        name: 'fileDisplayTest.txt',
+        address: '12MB',
+      },
+      {
+        date: '2016-05-07',
+        name: 'fileDisplayTest.txt',
+        address: '12MB',
+      },
+      {
+        date: '2016-05-07',
+        name: 'fileDisplayTest.txt',
+        address: '12MB',
+      },
+      {
+        date: '2016-05-07',
+        name: 'fileDisplayTest.txt',
+        address: '12MB',
+      },
+      {
+        date: '2016-05-07',
+        name: 'fileDisplayTest.txt',
+        address: '12MB',
+      },
+      {
+        date: '2016-05-07',
+        name: 'fileDisplayTest.txt',
+        address: '12MB',
+      },
+    ];
 
     const getHomeDirFiles = () => {
       let root = store.state.user.user_drive_root_url;
@@ -79,28 +113,15 @@ export default {
       });
     };
 
-    const uploadFile = (file, fileList) => {
-      console.log(file);
-      console.log(fileList);
-      let data = new FormData();
-      data.append("myfile", file);
-      let directory = "photo"
-
-      $.ajax({
-        url: "http://192.168.31.203:8066/upload/?directory=" + directory + "&token=" + store.state.user.access,
-        type: "POST",
-        data: data,
-        cache: false,
-        processData: false,
-        contentType: false,
-      });
-    };
+    let directory = "photo"
+    const uploadUrl = "http://192.168.31.203:8066/upload/?directory=" + directory + "&token=" + store.state.user.access;
 
     return {
       getHomeDirFiles,
       homeDirList,
       homeFileList,
-      uploadFile,
+      uploadUrl,
+      tableData,
     }
   }
 }
