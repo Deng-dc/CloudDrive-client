2ed2fd5650fdbb700287b55b34c2131c6c7cc53b
DengCong <dengc99@163.com>
2022-09-24 23:38:51 +0800

    dynamic diaplay distination dir files and breadcrumb

--- src/components/ContentArea.vue
+++ src/components/ContentArea.vue
@@ -11,25 +11,22 @@
   </div>
   <div class="card-body path-navigate">
     <el-breadcrumb separator="/" style="cursor: pointer;">
-      <el-breadcrumb-item @click="getHomeDirFiles">全部文件</el-breadcrumb-item>
-      <el-breadcrumb-item>promotion management</el-breadcrumb-item>
-      <el-breadcrumb-item>promotion list</el-breadcrumb-item>
-      <el-breadcrumb-item>promotion detail</el-breadcrumb-item>
+      <el-breadcrumb-item @click="getDistDirFiles">全部文件</el-breadcrumb-item>
+      <el-breadcrumb-item v-for="(item, index) in currentDir.path" :key="index" @click="getDistDirFiles">
+        {{item}}
+      </el-breadcrumb-item>
     </el-breadcrumb>
   </div>
   <div class="card-body content-area">
-    <el-table ref="multipleTableRef" :data="tableData" height="250" style="width: 100%"
-      @selection-change="handleSelectionChange">
+    <el-table ref="multipleTableRef" :data="tableData" height="250" style="width: 100%" @row-click="clickItem">
       <el-table-column type="selection" width="55" />
-      <el-table-column property="name" label="文件名" width="400" />
-      <el-table-column label="修改日期" width="200">
-        <template #default="scope">{{ scope.row.date }}</template>
-      </el-table-column>
-      <el-table-column property="address" label="大小" width="120" />
+      <el-table-column property="filename" label="文件名" width="400" />
+      <el-table-column property="lastModifyTime" label="修改日期" width="200" />
+      <el-table-column property="fileSize" label="大小" width="120" />
     </el-table>
   </div>
   <div class="card-body upload-area">
-    <el-upload class="upload-demo" drag :action="uploadUrl">
+    <el-upload class="upload-demo" drag :action="uploadUrl" :before-upload="getUploadDirectory">
       <el-icon class="el-icon--upload">
         <upload-filled />
       </el-icon>
@@ -42,6 +39,7 @@
 import $ from 'jquery';
 import { useStore } from 'vuex';
 import { reactive } from 'vue';
+import { ref } from 'vue';
 import { UploadFilled } from '@element-plus/icons-vue'
 
 
@@ -52,74 +50,77 @@ export default {
   },
   setup() {
     const store = useStore();
-    const file = reactive({
-      filename: "",
-      is_directory: "",
-      fileSize: "",
-      lastModifyTime: "",
+    let tableData = reactive([]);
+    let currentDir = reactive({
+      path: [],
     });
-    const tableData = [
-      {
-        date: '2016-05-07',
-        name: 'fileDisplayTest.txt',
-        address: '12MB',
-      },
-      {
-        date: '2016-05-07',
-        name: 'fileDisplayTest.txt',
-        address: '12MB',
-      },
-      {
-        date: '2016-05-07',
-        name: 'fileDisplayTest.txt',
-        address: '12MB',
-      },
-      {
-        date: '2016-05-07',
-        name: 'fileDisplayTest.txt',
-        address: '12MB',
-      },
-      {
-        date: '2016-05-07',
-        name: 'fileDisplayTest.txt',
-        address: '12MB',
-      },
-      {
-        date: '2016-05-07',
-        name: 'fileDisplayTest.txt',
-        address: '12MB',
-      },
-    ];
 
-    const getHomeDirFiles = () => {
-      let root = store.state.user.user_drive_root_url;
+    const getDistDirFiles = () => {
+      let requestUrl = store.state.user.user_drive_root_url + "?path=" + "";
+      console.log("requestUrl : " + requestUrl);
       $.ajax({
-        url: root,
+        url: requestUrl,
         type: "GET",
         dataType: "json",
         contentType: "application/json",
         success(resp) {
-          console.log(resp);
-          console.log(resp.data);
-          console.log(resp.data[0]);
-          console.log(resp.data[0].filename);
+          let fileList = resp.data;
+          // 每次都需要先清空tableData的数据
+          tableData.splice(0, tableData.length);
+          for (let i = 0; i < fileList.length; i++) {
+            tableData.push(fileList[i]);
+          }
+          console.log(tableData);
         }
       });
     };
 
-    const handleSelectionChange = () => {
-      console.log("handleSelectionChange");
+    let uploadUrl = ref('');
+
+    const getUploadDirectory = () => {
+      console.log("into before-upload");
+      // let directory = "";
+      // for (let dir of currentDir.path) {
+      //   directory = directory + dir + "\\";
+      // }
+      // console.log("directory : " + directory);
+      uploadUrl.value = "http://192.168.100.7:8066/upload/?directory=" + currentDir.path + "&token=" + store.state.user.access;
+      console.log("getUploadDirectory uploadUrl : " + uploadUrl.value);
     }
 
-    let directory = "photo"
-    const uploadUrl = "http://192.168.31.203:8066/upload/?directory=" + directory + "&token=" + store.state.user.access;
+    const clickItem = (row) => {
+      if (row.fileSize === "-") {
+        // 仅当是目录的时候才能进入下一个目录
+        currentDir.path.push(row.filename);
+        let requestUrl = store.state.user.user_drive_root_url + "?path=" + currentDir.path;
+        console.log("requestUrl : " + requestUrl);
+        $.ajax({
+          url: requestUrl,
+          type: "GET",
+          dataType: "json",
+          contentType: "application/json",
+          success(resp) {
+            let fileList = resp.data;
+            tableData.splice(0, tableData.length);
+            for (let i = 0; i < fileList.length; i++) {
+              tableData.push(fileList[i]);
+            }
+            console.log(tableData);
+          },
+        });
+      } else {
+        console.log("文件暂不支持在线打开");
+      }
+    }
 
     return {
-      getHomeDirFiles,
+      getDistDirFiles,
       uploadUrl,
-      file,
+      // file,
       tableData,
-      handleSelectionChange,
+      clickItem,
+      currentDir,
+      getUploadDirectory,
     }
   }
 }
